80d19c4d2d768c1173531f9f676291113fd0565a
Dylan Feng <dylanfeng@Dylans-MacBook-Pro.local>
2020-05-08 15:01:38 -0700

    Headline Creator

--- DBR-JS-7.3.0.2/example/web/angular/src/app/Dynamsoft.ts
+++
@@ -1,6 +0,0 @@
-import Dynamsoft from "dynamsoft-javascript-barcode";
-Dynamsoft.BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@7.3.0-v2/dist/";
-// Please visit https://www.dynamsoft.com/CustomerPortal/Portal/TrialLicense.aspx to get a trial license
-Dynamsoft.BarcodeReader.productKeys = "t0068NQAAAHDVW6dGL/vdS8Bvh5eoyf6I4u56DGi1aP48vvQYz3c0qjtp9ytYEBDIw6KoBT/XtiPXpnssdq6a9rZT3jly2XI=";
-// Dynamsoft.BarcodeReader._bUseFullFeature = true; // Control of loading min wasm or full wasm.
-export default Dynamsoft;
--- DBR-JS-7.3.0.2/example/web/vue/src/Dynamsoft.js
+++
@@ -1,6 +0,0 @@
-import Dynamsoft from "dynamsoft-javascript-barcode";
-Dynamsoft.BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@7.3.0-v2/dist/";
-// Please visit https://www.dynamsoft.com/CustomerPortal/Portal/TrialLicense.aspx to get a trial license
-Dynamsoft.BarcodeReader.productKeys = "t0068NQAAAJ1TkVG5jzSa+27rHrisnx2Z3NcAGt1OYWTTswaAhg25CdBYCUBd/dAkCkJ5WS6O0uYLN3ErRtO7PET3+WrK2zw=";
-// Dynamsoft.BarcodeReader._bUseFullFeature = true; // Control of loading min wasm or full wasm.
-export default Dynamsoft;
